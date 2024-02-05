507a7a801af2130069746492bad5d3ec7106c257
Kostyantyn Kosyuk <kostyantyn.kosyuk@gmail.com>
2024-02-05 09:35:49 +0200

    fix: fixed path to component

--- src/components/animation-block/config.js
+++ src/components/animation-block/config.js
@@ -1,5 +1,5 @@
 export const animationBlock = {
   tag: 'animation-block',
-  src: '@gudhub/ssg-web-component-library/src/components/animation-block/animation-block.js',
+  src: '@gudhub/ssg-web-components-library/src/components/animation-block/animation-block.js',
   serverOnly: false
 }
