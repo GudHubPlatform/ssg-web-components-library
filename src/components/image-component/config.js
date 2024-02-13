bc94009a4fc25bbd54cb25d11e9af2d62e335443
Kostyantyn Kosyuk <kostyantyn.kosyuk@gmail.com>
2024-02-13 16:35:08 +0200

    chore: changed export of components

--- src/components/image-component/config.js
+++ src/components/image-component/config.js
@@ -1,5 +1,5 @@
 export const imageComponent = {
   tag: 'image-component',
-  src: '@gudhub/ssg-web-components-library/src/components/image-component/config.js',
+  src: '@gudhub/ssg-web-components-library/src/components/image-component/image-component.js',
   serverOnly: false
 }
