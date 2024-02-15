cbf7737cd33995c41a2ab94eff13557ce95c15ad
Kostyantyn Kosyuk <kostyantyn.kosyuk@gmail.com>
2024-02-15 17:34:43 +0200

    chore: removed console.log

--- src/components/text-only/text-only.js
+++ src/components/text-only/text-only.js
@@ -7,7 +7,6 @@ class TextOnly extends GHComponent {
     constructor() {
         super();
         super.setDefaultData(jsonTemplate);
-        console.log(111);
     }
 
     async onServerRender() {
