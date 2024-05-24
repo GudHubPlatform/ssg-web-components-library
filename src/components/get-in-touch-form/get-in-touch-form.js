07d8afd35222794d8607a51498f758ade0855a7c
Maksym Kos <maxkos18@gmail.com>
2024-05-24 12:04:34 +0300

    fix: remove hardcoded fetch from form, small refactor

--- src/components/get-in-touch-form/get-in-touch-form.js
+++ src/components/get-in-touch-form/get-in-touch-form.js
@@ -1,10 +1,9 @@
 import html from './get-in-touch-form.html';
 import './get-in-touch-form.scss';
-import { sendEmail } from './sendEmail.js';
 import defaultConfigs from './get-in-touch-form-data.json';
 
 class GetInTouchForm extends GHComponent {
-    
+
     constructor() {
         super();
         this.formId = this.getAttribute("data-form-id");
@@ -21,7 +20,7 @@ class GetInTouchForm extends GHComponent {
         if (this.hasAttribute('data-in-popup')) {
             return;
         }
-        
+
         this.initConfig(this.config);
         super.render(html);
     }
@@ -40,7 +39,7 @@ class GetInTouchForm extends GHComponent {
     }
 
     attachEventListeners() {
-        this.getElementsByTagName('form')[0].addEventListener('submit', (e) => this.handleSubmit(e.target));
+        this.getElementsByTagName('form')[0].addEventListener('submit', (e) => this.handleSubmit(e));
         this.getElementsByClassName('restart_button')[0].addEventListener('click', (e) => this.hideFail());
     }
 
@@ -55,55 +54,87 @@ class GetInTouchForm extends GHComponent {
 
     initConfig(formConfigs) {
         try {
-            this.config = formConfigs.find(({id}) => id === this.formId);
+            this.config = formConfigs.find(({ id }) => id === this.formId);
             if (!this.config) {
-                throw e;
+                throw new Error("Config not found");
             }
         } catch (error) {
             const defaultId = this.isInPopup ? 'default popup' : 'default';
-            this.config = defaultConfigs.find(({id}) => id === defaultId);
+            this.config = defaultConfigs.find(({ id }) => id === defaultId);
         }
     }
 
-    async handleSubmit(element) {
+    validateInput(inputElement, regex, isRequired) {
+        const value = inputElement.value;
+        let isValid = true;
+
+        if (isRequired) {
+            isValid = regex.test(value);
+        } else if (value.length > 0) {
+            isValid = regex.test(value);
+        }
+
+        if (!isValid) {
+            inputElement.classList.add('error');
+            inputElement.parentElement.classList.add('error-input');
+        } else {
+            inputElement.classList.remove('error');
+            inputElement.parentElement.classList.remove('error-input');
+        }
+        
+        return isValid;
+    }
+
+    async emailAndPhoneValidation(emailInput, phoneInput) {
+        const emailConfig = this.config.inputs.find(input => input.name === "email");
+        const phoneConfig = this.config.inputs.find(input => input.name === "phone");
+    
+        const isEmailRequired = emailConfig?.required === 'true';
+        const isPhoneRequired = phoneConfig?.required === 'true';
+
+        let emailValid = true;
+        let phoneValid = true;
+    
+        if (emailInput) emailValid = this.validateInput(emailInput, /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, isEmailRequired);
+        
+        if (phoneInput) phoneValid = this.validateInput(phoneInput, /^[0-9 ()+-]+$/, isPhoneRequired);
+        
+        const isValid = (emailInput && emailValid) || (phoneInput && phoneValid);
+    
+        if (isEmailRequired || isPhoneRequired) return emailValid && phoneValid;
+        
+        return isValid;
+    }
+
+    async handleSubmit(event) {
         event.preventDefault();
-
+        const element = event.target;
+    
         const emailInput = this.querySelector('[name="email"]');
-        const email = emailInput ? emailInput.value : '';
-        
         const phoneInput = this.querySelector('[name="phone"]');
-        const phone = phoneInput ? phoneInput.value || '' : '';
-        
-        const isValidFields = this.validation(email, phone);
-
-        if (isValidFields.phoneValid && isValidFields.emailValid) {
+    
+        const isValidFields = await this.emailAndPhoneValidation(emailInput, phoneInput);
+    
+        if (isValidFields) {
             this.addLoader();
-
-            const TIMEOUT_DURATION = 5000;
-
-            const sendEmailWithTimeout = (element, config, placement) => {
-                return new Promise((resolve, reject) => {
-                    const timer = setTimeout(() => {
-                        reject();
-                    }, TIMEOUT_DURATION);
-
-                    sendEmail(element, config, placement)
-                        .then((res) => {
-                            clearTimeout(timer);
-                            resolve(res);
-                        })
-                        .catch((error) => {
-                            clearTimeout(timer);
-                            reject(error);
-                        });
-                });
-            };
-
             try {
-                const res = await sendEmailWithTimeout(element, this.config, this.placement);
+                const res = await new Promise((resolve, reject) => {
+                    const TIMEOUT_DURATION = 2000;
+    
+                    setTimeout(() => {
+                        const isSuccess = true; 
+                        if (isSuccess) {
+                            resolve(true);
+                        } else {
+                            reject(new Error('Failed to send email'));
+                        }
+                    }, TIMEOUT_DURATION);
+                });
+    
                 this.removeLoader(element);
+    
                 if (res) {
-                    this.showSuccess({ email, phone });
+                    this.showSuccess({ email: emailInput ? emailInput.value : '', phone: phoneInput ? phoneInput.value : '' });
                 } else {
                     this.showFail();
                 }
@@ -112,37 +143,37 @@ class GetInTouchForm extends GHComponent {
                 this.showFail();
             }
             this.isFormSubmitted = true;
-
+    
         } else {
             this.toggleErrors(isValidFields, emailInput, phoneInput);
         }
-    }
 
+        this.createDataObject(element, this.config, this.placement)
+    }
+    
+    async createDataObject(form, formId, placement) {
+        const formData = {};
+        for (const [name, value] of (new FormData(form)).entries()) {
+            formData[name] = value;
+        }
+
+        const formDataObj = {
+            Website: window.location.hostname,
+            Url: window.location.pathname,
+            FormId: formId,
+            FormPlacement: placement,
+            FormData: {
+                ...formData
+            },
+            Referrer: localStorage.getItem('referrer'),
+        };
+
+        window.dispatchEvent(new CustomEvent('submitForm', { detail: { formDataObj } }));
+    }
     toggleErrors(isValidFields, emailInput, phoneInput) {
         isValidFields.emailValid ? emailInput.classList.remove('error') : emailInput.classList.add('error');
-            
+
         isValidFields.phoneValid ? phoneInput.classList.remove('error') : phoneInput.classList.add('error');
-    }
-
-    validation (email = '', phone = '') {
-        const isPhoneRequired = this.config.inputs.find(input => input.name === "phone").required;
-        const isEmailRequired = this.config.inputs.find(input => input.name === "email").required;
-        
-        let emailValid;
-        if (email.length === 0 && isEmailRequired == 'false') {
-            emailValid = true;
-        } else {
-            emailValid = /\S+@\S+\.\S+/.test(email);
-        }
-        
-        let phoneValid;
-        if (phone.length === 0 && isPhoneRequired == 'false') {
-            phoneValid = true;
-        } else {
-            phoneValid = /^\+?[\d()-\s]+$/.test(phone);
-        }
-
-        return { phoneValid, emailValid };
     }
     async addLoader() {
         this.classList.add('loading');
@@ -156,7 +187,11 @@ class GetInTouchForm extends GHComponent {
         }, 500);
     }
     async showSuccess({email, phone}) {
-        this.getElementsByClassName('email')[0].innerText = email;
+        if (email) {
+            this.querySelector('.check_entity').classList.add('provided');
+            this.getElementsByClassName('email')[0].innerText = email;
+        }
+        this.classList.add('success');
         if (phone) {
             this.querySelector('.check_entity.phone_entity').classList.add('provided');
             this.getElementsByClassName('phone')[0].innerText = phone;
@@ -180,7 +215,6 @@ class GetInTouchForm extends GHComponent {
             overflowFail.style.opacity = '';
         }, 500);
     }
-
     generateInput(config) {
         return config.inputs.reduce((acc, input) => {
             const maxSymbols = {
@@ -194,13 +228,14 @@ class GetInTouchForm extends GHComponent {
             const maxLength = tag === 'textarea' ? maxSymbols.long : maxSymbols[input.type];
             return acc + `
                 <div class="input-wrap col-${input.width}">
-                    <${tag} type="text" name=${input.name} placeholder="${input.placeholder}" ${JSON.parse(input.required) ? 'required' : ''} ${ maxLength ? `maxlength=${maxLength}` : ''}></${tag}>
+                    <${tag} type="text" name=${input.name} placeholder="${input.placeholder}" ${JSON.parse(input.required) ? 'required' : ''} ${maxLength ? `maxlength=${maxLength}` : ''}></${tag}>
                     ${input.type === 'email' || input.type === 'phone' ? `<span class="${input.type}-error">${input.errorText}</span>` : ''}
                 </div>
-            `}, '');
+            `;
+        }, '');
     }
 }
 
-if(!customElements.get('get-in-touch-form')) {
+if (!customElements.get('get-in-touch-form')) {
     customElements.define('get-in-touch-form', GetInTouchForm);
 }
--- src/components/get-in-touch-form/get-in-touch-form.readme.md
+++ src/components/get-in-touch-form/get-in-touch-form.readme.md
@@ -1,18 +1,24 @@
-# Config: 
+# Config:
+
 The form field settings are defined in the site's config.mjs, when rendered on the server it is stored in "window.getConfig().componentsConfigs.form_config", and this config is also defined in the client's "window.getConfig().componentsConfigs.formConfig" and is used when rendering the form on the client. If the config in "window" is not available, then the config is taken from "get-in-touch-form-data.json". There can be several settings for forms, the form selects it by id (it is defined in the "data-form-id" attribute of the form component), this allows you to customize the fields of several forms in different ways (for example, on the page the form has all the fields, and in the popup the form has other types of fields)
 
 # Form in popup:
+
 if form will be rendered in popup we need to define attribute "data-in-popup", that will change some styles of form to fit the popup styles.
 
 # Placement:
+
 The "placement" variable is needed to track conversions. If the form is in a popup, then "placement" determined by the button that opened the popup with the form. If the form is already on the page, this value is defined in the form constructor (currently "this.placement = 'main' ").
 
 # Data-attributes:
+
 data-in-popup: if form is on popup
 data-form-id="form-id": determine id of config that will be applyed to form
 
-# Config object: 
+# Config object:
+
 ("?" means "unnecessary")
+
 ```json
 {
         "id": "string",
@@ -26,6 +32,7 @@ data-form-id="form-id": determine id of config that will be applyed to form
             "from": "string",
             "subject": "string"
         },
+        "endpointForEmails": "some fancy url for gudhub API",
         "inputs": [
             {
                 "name": "string",
@@ -37,7 +44,9 @@ data-form-id="form-id": determine id of config that will be applyed to form
         ]
     },
 ```
+
 ## Types description:
+
 email: will be checked by email rules;
 phone: will be checked by phone number rules;
 short: max length 64 symbols;
@@ -45,4 +54,51 @@ long: max length 128 symbols;
 textarea: tag "textarea";
 
 ## Width:
+
 defines the width of the field in the row. The total width of the row is 12. That is, if 2 fields have a width of 6, then they will take up half of the line each
+
+## Correct way to handle submit in your project
+
+```js
+// by default this event handled in your main.js
+window.addEventListener("submitForm", async (event) => {
+  const { formDataObj } = event.detail;
+
+  // add other code here
+});
+```
+
+### What data contains in formDataObj
+
+```js
+{
+FormData :
+    // all data from your form inputs on website
+    name : "name"
+    phone: "phhone"
+    email: 'email'
+FormId:
+    // data from your form-config
+    button_text:"text"
+    defaultLang: "true or false"
+    endpointForEmails: "endpoint to gudhub app"
+    id: "text"
+    inputs: Array()
+        0: {name: 'name', type: 'type', required: 'required', placeholder: "placeholder", width: 12}
+        1: {name: 'name', type: 'type', required: 'required', placeholder: 'placeholder *', errorText: 'errorText', …}
+        langCode: "text"
+    mailConfig:
+        from: "from"
+        subject: "subject"
+        to: "to"
+        subtitle: "subtitle"
+        subtitleSuccess: "subtitleSuccess"
+        title: "title"
+        titleFail:"titleFail"
+        titleSuccess: "titleSuccess"
+FormPlacement: "FormPlacement"
+Referrer: "Referrer"
+Url: "/"
+Website: "Website"
+}
+```
