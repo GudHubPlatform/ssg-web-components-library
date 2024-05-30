9962293e89c78d8ba0fd9986f3915ab2068e92b4
Vitalik0001 <vitalik.n1803@gmail.com>
2024-05-30 16:05:06 +0300

    fix: added config files

---
+++ src/components/masonry-gallery/config.js
@@ -0,0 +1,5 @@
+export const masonryGallery = {
+    tag: 'masonry-gallery',
+    src: '@gudhub/ssg-web-components-library/src/components/masonry-gallery/masonry-gallery.js',
+    serverOnly: false
+  }
