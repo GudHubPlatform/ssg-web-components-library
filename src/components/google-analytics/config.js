f0e84cd843d410a63a21983b32528db923f75ccd
Max <maksymkosfe.developer@gmail.com>
2025-05-12 10:36:56 +0300

    feature: add google-analytics component

---
+++ src/components/google-analytics/config.js
@@ -0,0 +1,6 @@
+export const GoogleAnalytics = {
+  tag: 'google-analytics',
+  category: 'Utilities',
+  src: '@gudhub/ssg-web-components-library/src/components/google-analytics/google-analytics.js',
+  serverOnly: false
+}
