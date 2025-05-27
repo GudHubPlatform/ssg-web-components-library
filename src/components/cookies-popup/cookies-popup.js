7e6572be2f50a690e2cb4ff40364c9f4a681513d
Max <maksymkosfe.developer@gmail.com>
2025-05-27 13:59:43 +0300

    feature: update cookies-popup

--- src/components/cookies-popup/cookies-popup.js
+++ src/components/cookies-popup/cookies-popup.js
@@ -1,27 +1,70 @@
-import html from './cookies-popup.html';
-import './cookies-popup.scss';
+import html from "./cookies-popup.html";
+import "./cookies-popup.scss";
 
 class CookiesPopup extends GHComponent {
-    
     constructor() {
         super();
+        this.inEU = false;
+        fetch("https://ipapi.co/json/").then((response) => response.json()).then((data) => this.inEU = data.in_eu);
     }
 
     async onServerRender() {
         super.render(html);
     }
 
-    onClientReady () {
-        if ( localStorage.getItem('agreeWithCookies') == null ) {
-            this.classList.add('show');
+    async onClientReady() {
+        if (this.inEU) {
+            if (localStorage.getItem("agreeWithCookies") === false) {
+
+                this.classList.add("show");
+
+                const script = document.createElement('script');
+                script.innerHTML = `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('consent', 'default', {
+                        'ad_storage': 'denied',
+                        'analytics_storage': 'denied',
+                        'functionality_storage': 'denied',
+                        'security_storage': 'granted',
+                        'wait_for_update': 500
+                    });
+                `;
+
+                document.querySelector('head').prepend(script);
+
+            } else {
+                setTimeout(() => {
+
+                    gtag('consent', 'update', {
+                        'ad_storage': 'granted',
+                        'analytics_storage': 'granted',
+                        'functionality_storage': 'granted',
+                        'security_storage': 'granted',
+                        'personalization_storage': 'granted',
+                        'wait_for_update': 500
+                    });
+
+                }, 3500);
+            }
         }
+
     }
 
     gotIt() {
-        this.classList.add('hide');
-        localStorage.setItem('agreeWithCookies', true);
-    }
+        localStorage.setItem("agreeWithCookies", "true");
+        this.classList.add("hide");
 
+        gtag('consent', 'update', {
+            'ad_storage': 'granted',
+            'analytics_storage': 'granted',
+            'functionality_storage': 'granted',
+            'security_storage': 'granted',
+            'personalization_storage': 'granted',
+            'wait_for_update': 500
+        });
+
+    }
 }
 
-window.customElements.define('cookies-popup', CookiesPopup);
+window.customElements.define("cookies-popup", CookiesPopup);
